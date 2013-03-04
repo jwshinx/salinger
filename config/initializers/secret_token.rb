54bd4c83e37866c45317c06b144985a501f20f8f
Joel Shin <jwshin@gmail.com>
2013-03-04 00:32:11 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salinger::Application.config.secret_token = '02435084019923b7ab3a2687c7c7becdeb42925acc3e9927f1a84540fc90d76ede9d5c65b768b0beffa4a998b3ddad69263392e5dd284d3e8c809b3ccc3deab9'
