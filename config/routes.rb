4ec482b4f0749c09f23c2e92e640cab49b68eb09
Joel Shin <jwshin@gmail.com>
2013-03-06 00:04:14 +0000

    added under-construction pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Salinger::Application.routes.draw do
+  match "incubating_one" => "home#incubating_one"
+  match "incubating_two" => "home#incubating_two"
+  match "incubating_three" => "home#incubating_three"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Salinger::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
