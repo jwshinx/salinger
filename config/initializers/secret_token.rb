72880e0795b90dae07ec748928c1532c3216187c
Joel Shin <jwshin@gmail.com>
2013-03-07 06:17:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unido::Application.config.secret_token = '5c47dce73ce954e0a5f20781b47a65bd62c2fd8b1cf408d2473570235f1b527a50cd2cf9dbde8571ab8fc828d33443b7bda00b48ede6461e96a42cc139eccc43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Unido::Application.config.session_store :cookie_store, key: '_unido_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Unido::Application.config.session_store :active_record_store
