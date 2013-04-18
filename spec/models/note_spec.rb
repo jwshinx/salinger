04950e6eb74589670dff3cad96980e093148e89f
Joel Shin <jwshin@gmail.com>
2013-04-18 22:27:00 +0000

    rfactrd note spec

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 require 'cancan/matchers'
 
 describe Note do
+
+ shared_examples_for "customer owner" do
+  it "is Mark Twain" do
+   "#{subject.owner.firstname} #{subject.owner.lastname}".should == 'Mark Twain'
+  end
+ end
+
  describe "fyi" do
   subject { FactoryGirl.create(:customer_fyi) }
   its(:content) { should == 'remember to mark it' }
-  it "belongs to customer mark twain" do
-   "#{subject.owner.firstname} #{subject.owner.lastname}".should == 'Mark Twain'
-  end
+  it_should_behave_like "customer owner"
  end
  describe "todo" do
   subject { FactoryGirl.create(:customer_todo) }
   its(:content) { should == 'send him bill' }
-  it "belongs to customer mark twain" do
-   "#{subject.owner.firstname} #{subject.owner.lastname}".should == 'Mark Twain'
-  end
+  it_should_behave_like "customer owner"
  end
 end
