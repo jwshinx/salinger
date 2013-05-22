75229799aa7ad3cdb27e4cedeec8d57bdbacff88
Joel Shin <jwshin@gmail.com>
2013-05-22 20:00:00 +0000

    cleandup views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+ def and_show_count_of collection
+  return "No #{params[:controller].capitalize}" if collection.empty?
+  "#{params[:controller].capitalize} (#{collection.length})"
+ end
  def cents_to_dollars amount
   return 0 if amount.nil? || amount =~ /[^\d]/
   amount.to_i / 100.0
