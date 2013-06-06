661aadfbc7f757da4c438e0812a11596df7373f4
Joel Shin <jwshin@gmail.com>
2013-06-06 02:04:11 +0000

    order purchase date client side validation added

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,7 @@ class Order < ActiveRecord::Base
  belongs_to :creator, :class_name => "User", :foreign_key => "created_by"
  belongs_to :updater, :class_name => "User", :foreign_key => "updated_by"
  belongs_to :customer
+ #validates :purchase_date, :presence => true, :length => { :minimum => 1 }
+ validates :purchase_date, :presence => true
  accepts_nested_attributes_for :line_items, :reject_if => lambda { |a| a[:product_id].blank? }, :allow_destroy => true
 end
