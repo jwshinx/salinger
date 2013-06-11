a822ed4decf21e8513858a8a7c1af6682f2c564b
Joel Shin <jwshin@gmail.com>
2013-06-11 04:47:43 +0000

    state abbrevs added to customer-address form

--- lib/my_constants.rb
+++ lib/my_constants.rb
@@ -1,20 +1,6 @@
-module ApplicationHelper
- def is_admin_joel?( user )
-  ( user.username == 'joel' && user.role.name == 'admin' ) ? true : false
- end
- def and_show_count_of collection
-  return "No #{params[:controller].capitalize}" if collection.empty?
-  "#{params[:controller].split('_').map{|t|t.capitalize}.join(' ')} (#{collection.length})"
- end
- def cents_to_dollars amount
-  return 0 if amount.nil? || amount =~ /[^\d]/
-  amount.to_i / 100.0
- end
- def mmddyy_date date
-  date.nil? ? '' : date.strftime("%m/%d/%Y")
- end
- def us_states
-    [
+module MyConstants
+ US_STATES =  
+  [
       ['Alabama', 'AL'],
       ['Alaska', 'AK'],
       ['Arizona', 'AZ'],
@@ -67,6 +53,5 @@ module ApplicationHelper
       ['West Virginia', 'WV'],
       ['Wisconsin', 'WI'],
       ['Wyoming', 'WY']
-    ]
- end
+ ]
 end
