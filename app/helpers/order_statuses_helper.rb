5ef63d144f343a2641bd023859008690c7fd25be
Joel Shin <jwshin@gmail.com>
2013-06-26 19:18:10 +0000

    added view helper make-it-a-link

--- app/helpers/order_statuses_helper.rb
+++ app/helpers/order_statuses_helper.rb
@@ -1,2 +1,9 @@
 module OrderStatusesHelper
+ def make_link display_text, model, ability, object, url_token
+  if can? ability, model
+   content_tag(:span, display_text)
+  else
+   content_tag(:a, display_text, :href => "/#{url_token}/#{object.id}") 
+  end
+ end
 end
