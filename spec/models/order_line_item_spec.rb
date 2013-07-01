8f08ae2a3f37892f51b7afad5b8848d2078da1e9
Joel Shin <jwshin@gmail.com>
2013-07-01 08:06:49 +0000

    rfactrd order line item specs

--- spec/models/order_line_item_spec.rb
+++ spec/models/order_line_item_spec.rb
@@ -1,26 +1,41 @@
 require 'spec_helper'
+require 'cancan/matchers'
 
 describe OrderLineItem do
- subject { FactoryGirl.create(:lebron).orders.first.line_items.first }
- its(:quantity) { should == 1 }
- its(:price) { should == 2000 }
- its(:subtotal) { should == 2000 }
- it "returns creator" do
-  subject.creator.username.should =~ /joel/
+ before do
+  @li = OrderLineItem.new({created_by:1, updated_by:1})
+  @qty = random_number; @li.quantity = @qty
+  @price = random_number; @li.price = @price
+  @subtotal = random_number; @li.subtotal = @subtotal
  end
- it "returns updater" do
-  subject.updater.username.should =~ /joel/
- end
-
+ subject { @li }
+ specify { subject.should be_valid }
+ its(:quantity) { should == @qty.to_i }
+ its(:price) { should == @price.to_i }
+ its(:subtotal) { should == @subtotal.to_i }
  describe "privileges" do
-  it "admin" do
-   pending
+  subject { my_ability }
+  let(:my_ability) { Ability.new(@user) }
+  describe "for admin" do
+   before(:each) { @user = FactoryGirl.create(:admin_user) }
+   it { should be_able_to(:read, OrderLineItem.new) }
+   it { should be_able_to(:update, OrderLineItem.new) }
+   it { should be_able_to(:create, OrderLineItem.new) }
+   it { should be_able_to(:destroy, OrderLineItem.new) }
   end
-  it "manager" do
-   pending
+  describe "for manager" do
+   before(:each) { @user = FactoryGirl.create(:manager_user) }
+   it { should be_able_to(:read, OrderLineItem.new) }
+   it { should be_able_to(:update, OrderLineItem.new) }
+   it { should be_able_to(:create, OrderLineItem.new) }
+   it { should be_able_to(:destroy, OrderLineItem.new) }
   end
-  it "member" do
-   pending
+  describe "for all else" do
+   before(:each) { @user = FactoryGirl.create(:member_user) }
+   it { should_not be_able_to(:read, OrderLineItem.new) }
+   it { should_not be_able_to(:update, OrderLineItem.new) }
+   it { should_not be_able_to(:create, OrderLineItem.new) }
+   it { should_not be_able_to(:destroy, OrderLineItem.new) }
   end
  end
 end
