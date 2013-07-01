231e560622f2e13f8d22561f2ca94288dca05a1c
Joel Shin <jwshin@gmail.com>
2013-07-01 07:13:11 +0000

    rfactrd product specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,89 @@ require "spec_helper"
 require 'cancan/matchers'
 
 describe Product do
+ describe "normally" do
+  before do 
+   @product = Product.new({created_by: 1, updated_by:1}) 
+   @name = random_string
+   @product.name = @name
+   @description = random_string
+   @product.description = @description
+   @count = random_number
+   @product.count = @count
+   @price = random_number
+   @product.price = @price
+  end
+  subject { @product }
+  specify { subject.should be_valid } 
+  its(:description) { should == @description }
+  its(:count) { should == @count.to_i }
+  its(:price) { should == @price.to_i }
+  describe "when name" do
+   it "is blank throws exception" do 
+    @product.name = '' 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Name is too short/) 
+   end
+   it "is nil throws exception" do 
+    @product.name = nil 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Name can't be blank/) 
+   end
+  end
+  describe "when description" do
+   it "is blank throws exception" do 
+    @product.description = '' 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Description is too short/) 
+   end
+   it "is nil throws exception" do 
+    @product.description = nil 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Description can't be blank/) 
+   end
+  end
+  describe "when price" do
+   it "is nil throws exception" do 
+    @product.price = nil 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Price is not a number/) 
+   end
+   it "is not numeric throws exception" do 
+    @product.price = random_string 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Price is not a number/) 
+   end
+   it "is not greater than zero throws exception" do 
+    @product.price = 0 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Price must be greater than 0/) 
+   end
+  end
+  describe "when non-numeric count" do
+   it "throws exception" do 
+    @product.count = random_string 
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Count is not a number/) 
+   end
+  end
+  describe "when price doesn't look like dollars" do
+   it "throws exception" do 
+    pending
+    @product.price = 1.8989898
+    expect { 
+     @product.save!
+    }.to raise_error(ActiveRecord::RecordInvalid, /Price xxx/) 
+   end
+  end
+ end
  describe "red solid" do
   let(:length) { FactoryGirl.create(:red_solid).fabrics.length }
   let(:fabric_name) { FactoryGirl.create(:red_solid).fabrics.first.name }
@@ -9,74 +92,6 @@ describe Product do
   it "has fabric red-cotton" do fabric_name.should == 'red cotton' end 
  end
 
- describe "when perfect" do
-  subject { FactoryGirl.create(:orange_argyle) }
-  specify { subject.should be_valid } 
-  its(:name) { should == 'orange argyle' }
-  its(:name) { should_not be_blank }
-  its(:description) { should == 'simple. comfy.' }
-  its(:description) { should_not be_blank }
-  its(:price) { should == 1999 }
-  its(:price) { should_not be_blank }
- end
- describe "when name" do
-  it "is blank throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :name => '') 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Name is too short/) 
-  end
-  it "is nil throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :name => nil) 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Name can't be blank/) 
-  end
- end
- describe "when description" do
-  it "is blank throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :description => '') 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Description is too short/) 
-  end
-  it "is nil throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :description => nil) 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Description can't be blank/) 
-  end
- end
- describe "when price" do
-  it "is nil throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :price => nil) 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Price is not a number/) 
-  end
-  it "is not numeric throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :price => 'sa') 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Price is not a number/) 
-  end
-  it "is not greater than zero throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :price => 0) 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Price must be greater than 0/) 
-  end
- end
- describe "when non-numeric count" do
-  it "throws exception" do 
-   expect { 
-    FactoryGirl.create(:orange_argyle, :count=> 'a') 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Count is not a number/) 
-  end
- end
- describe "when price doesn't look like dollars" do
-  it "throws exception" do 
-   pending
-   expect { 
-    FactoryGirl.create(:orange_argyle, :price => '1.018784278') 
-   }.to raise_error(ActiveRecord::RecordInvalid, /Price xxx/) 
-   a = FactoryGirl.create(:orange_argyle, :price => '888.000978') 
-   puts "---> #{a.errors.inspect}"
-  end
- end
  describe "privilege" do
   subject { my_ability }
   let(:my_ability) { Ability.new(@user) }
