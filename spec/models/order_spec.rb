cc47dc350562032cf21fa61d6efae24882348237
Joel Shin <jwshin@gmail.com>
2013-07-01 07:53:48 +0000

    rfactrd order specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,26 +1,52 @@
 require 'spec_helper'
+require 'cancan/matchers'
 
 describe Order do
- subject { FactoryGirl.create(:order) }
- its(:purchase_date) { should == Date.today }
- its(:purchase_amount) { should == 2500 }
+ before do
+  @order = Order.new( {created_by:1, updated_by:1}) 
+  @purchase_date = Date.today; @order.purchase_date = @purchase_date
+  @purchase_amount = random_number; @order.purchase_amount = @purchase_amount
+  @ispaid = false
+  #order_status_id
+  #customer_id
+ end
+ subject { @order } 
+ its(:purchase_date) { should == @purchase_date } 
+ its(:purchase_amount) { should == @purchase_amount.to_i } 
  its(:ispaid) { should be_false }
- it "returns creator" do 
-  subject.creator.username.should =~ /joel/
- end
- it "returns updater" do 
-  subject.updater.username.should =~ /joel/
- end
  
+ describe "with line items" do
+  it "returns count" do
+   line_item1 = mock_model OrderLineItem, id: 1 
+   line_item2 = mock_model OrderLineItem, id: 2
+   @order.line_items << line_item1 << line_item2
+   @order.line_items.length == 2
+  end
+ end
  describe "privileges" do
-  it "admin" do
-   pending
+  subject { my_ability }
+  let(:my_ability) { Ability.new(@user) }
+  describe "for admin" do
+   before(:each) { @user = FactoryGirl.create(:admin_user) }
+   it { should be_able_to(:read, Order.new) }
+   it { should be_able_to(:update, Order.new) }
+   it { should be_able_to(:create, Order.new) }
+   it { should be_able_to(:destroy, Order.new) }
   end
-  it "manager" do
-   pending
+  describe "for manager" do
+   before(:each) { @user = FactoryGirl.create(:manager_user) }
+   it { should be_able_to(:read, Order.new) }
+   it { should be_able_to(:update, Order.new) }
+   it { should be_able_to(:create, Order.new) }
+   it { should be_able_to(:destroy, Order.new) }
   end
-  it "member" do
-   pending
+  describe "for all else" do
+   before(:each) { @user = FactoryGirl.create(:member_user) }
+   it { should_not be_able_to(:read, Order.new) }
+   it { should_not be_able_to(:update, Order.new) }
+   it { should_not be_able_to(:create, Order.new) }
+   it { should_not be_able_to(:destroy, Order.new) }
   end
+
  end
 end
