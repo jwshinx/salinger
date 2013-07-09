12c8c70b6e38c69d9aaae8869c4cb839f5db8166
Joel Shin <jwshin@gmail.com>
2013-07-09 16:54:20 -0700

    rfactrd mspecs

--- spec/models/email_type_spec.rb
+++ spec/models/email_type_spec.rb
@@ -8,28 +8,28 @@ describe EmailType do
   its(:description) { should == 'signup for discounts' } 
  end 
  describe "privilege" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, EmailType.new) }
-   it { should be_able_to(:update, EmailType.new) }
-   it { should be_able_to(:create, EmailType.new) }
-   it { should be_able_to(:destroy, EmailType.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, EmailType.new) }
-   it { should_not be_able_to(:update, EmailType.new) }
-   it { should_not be_able_to(:create, EmailType.new) }
-   it { should_not be_able_to(:destroy, EmailType.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, EmailType.new) }
-   it { should_not be_able_to(:update, EmailType.new) }
-   it { should_not be_able_to(:create, EmailType.new) }
-   it { should_not be_able_to(:destroy, EmailType.new) }
-  end
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, EmailType.new) }
+    it { should be_able_to(:update, EmailType.new) }
+    it { should be_able_to(:create, EmailType.new) }
+    it { should be_able_to(:destroy, EmailType.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, EmailType.new) }
+    it { should_not be_able_to(:update, EmailType.new) }
+    it { should_not be_able_to(:create, EmailType.new) }
+    it { should_not be_able_to(:destroy, EmailType.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, EmailType.new) }
+    it { should_not be_able_to(:update, EmailType.new) }
+    it { should_not be_able_to(:create, EmailType.new) }
+    it { should_not be_able_to(:destroy, EmailType.new) }
+   end
  end
 end
