4efa5538c83de2bb3c6243d859e641559a682918
Joel Shin <jwshin@gmail.com>
2013-07-09 16:35:50 -0700

    rfactrd email mspecs

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -60,28 +60,28 @@ describe Email do
   end
  end
  describe "privilege" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, Email.new) }
-   it { should be_able_to(:update, Email.new) }
-   it { should be_able_to(:create, Email.new) }
-   it { should be_able_to(:destroy, Email.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, Email.new) }
-   it { should_not be_able_to(:update, Email.new) }
-   it { should be_able_to(:create, Email.new) }
-   it { should_not be_able_to(:destroy, Email.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, Email.new) }
-   it { should_not be_able_to(:update, Email.new) }
-   it { should_not be_able_to(:create, Email.new) }
-   it { should_not be_able_to(:destroy, Email.new) }
-  end
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, Email.new) }
+    it { should be_able_to(:update, Email.new) }
+    it { should be_able_to(:create, Email.new) }
+    it { should be_able_to(:destroy, Email.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, Email.new) }
+    it { should_not be_able_to(:update, Email.new) }
+    it { should be_able_to(:create, Email.new) }
+    it { should_not be_able_to(:destroy, Email.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, Email.new) }
+    it { should_not be_able_to(:update, Email.new) }
+    it { should_not be_able_to(:create, Email.new) }
+    it { should_not be_able_to(:destroy, Email.new) }
+   end
  end
 end
