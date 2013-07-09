76043f79c8376aa2ca38b87c3d79ea78c0cab5ac
Joel Shin <jwshin@gmail.com>
2013-07-09 15:38:03 -0700

    rfactrd role mspecs

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -35,28 +35,28 @@ describe Role do
   end
  end
  describe "privilege" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, Role.new) }
-   it { should be_able_to(:update, Role.new) }
-   it { should be_able_to(:create, Role.new) }
-   it { should be_able_to(:destroy, Role.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, Role.new) }
-   it { should_not be_able_to(:update, Role.new) }
-   it { should_not be_able_to(:create, Role.new) }
-   it { should_not be_able_to(:destroy, Role.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, Role.new) }
-   it { should_not be_able_to(:update, Role.new) }
-   it { should_not be_able_to(:create, Role.new) }
-   it { should_not be_able_to(:destroy, Role.new) }
-  end
- end
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, Role.new) }
+    it { should be_able_to(:update, Role.new) }
+    it { should be_able_to(:create, Role.new) }
+    it { should be_able_to(:destroy, Role.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, Role.new) }
+    it { should_not be_able_to(:update, Role.new) }
+    it { should_not be_able_to(:create, Role.new) }
+    it { should_not be_able_to(:destroy, Role.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, Role.new) }
+    it { should_not be_able_to(:update, Role.new) }
+    it { should_not be_able_to(:create, Role.new) }
+    it { should_not be_able_to(:destroy, Role.new) }
+   end
+ end 
 end
