db6b66ae418fbbb3b07d1f99353a29ababe27e7a
Joel Shin <jwshin@gmail.com>
2013-07-09 15:31:15 -0700

    rfactrd order line item mspecs

--- spec/models/order_line_item_spec.rb
+++ spec/models/order_line_item_spec.rb
@@ -13,29 +13,29 @@ describe OrderLineItem do
  its(:quantity) { should == @qty.to_i }
  its(:price) { should == @price.to_i }
  its(:subtotal) { should == @subtotal.to_i }
- describe "privileges" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, OrderLineItem.new) }
-   it { should be_able_to(:update, OrderLineItem.new) }
-   it { should be_able_to(:create, OrderLineItem.new) }
-   it { should be_able_to(:destroy, OrderLineItem.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, OrderLineItem.new) }
-   it { should be_able_to(:update, OrderLineItem.new) }
-   it { should be_able_to(:create, OrderLineItem.new) }
-   it { should be_able_to(:destroy, OrderLineItem.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, OrderLineItem.new) }
-   it { should_not be_able_to(:update, OrderLineItem.new) }
-   it { should_not be_able_to(:create, OrderLineItem.new) }
-   it { should_not be_able_to(:destroy, OrderLineItem.new) }
-  end
- end
+ describe "privilege" do
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, OrderLineItem.new) }
+    it { should be_able_to(:update, OrderLineItem.new) }
+    it { should be_able_to(:create, OrderLineItem.new) }
+    it { should be_able_to(:destroy, OrderLineItem.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, OrderLineItem.new) }
+    it { should be_able_to(:update, OrderLineItem.new) }
+    it { should be_able_to(:create, OrderLineItem.new) }
+    it { should be_able_to(:destroy, OrderLineItem.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, OrderLineItem.new) }
+    it { should_not be_able_to(:update, OrderLineItem.new) }
+    it { should_not be_able_to(:create, OrderLineItem.new) }
+    it { should_not be_able_to(:destroy, OrderLineItem.new) }
+   end
+ end 
 end
