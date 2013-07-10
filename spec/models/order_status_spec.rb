b91124d903d598fd4b70b6f8dfd633f724c72ac5
Joel Shin <jwshin@gmail.com>
2013-07-09 20:37:19 -0700

    rfactrd order status mspecs

--- spec/models/order_status_spec.rb
+++ spec/models/order_status_spec.rb
@@ -15,29 +15,28 @@ describe OrderStatus do
   its(:description) { should == @description }
  end
  describe "privilege" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, OrderStatus.new) }
-   it { should be_able_to(:update, OrderStatus.new) }
-   it { should be_able_to(:create, OrderStatus.new) }
-   it { should be_able_to(:destroy, OrderStatus.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, OrderStatus.new) }
-   it { should be_able_to(:update, OrderStatus.new) }
-   it { should be_able_to(:create, OrderStatus.new) }
-   it { should_not be_able_to(:destroy, OrderStatus.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, OrderStatus.new) }
-   it { should_not be_able_to(:update, OrderStatus.new) }
-   it { should_not be_able_to(:create, OrderStatus.new) }
-   it { should_not be_able_to(:destroy, OrderStatus.new) }
-  end
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, OrderStatus.new) }
+    it { should be_able_to(:update, OrderStatus.new) }
+    it { should be_able_to(:create, OrderStatus.new) }
+    it { should be_able_to(:destroy, OrderStatus.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, OrderStatus.new) }
+    it { should be_able_to(:update, OrderStatus.new) }
+    it { should be_able_to(:create, OrderStatus.new) }
+    it { should_not be_able_to(:destroy, OrderStatus.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, OrderStatus.new) }
+    it { should_not be_able_to(:update, OrderStatus.new) }
+    it { should_not be_able_to(:create, OrderStatus.new) }
+    it { should_not be_able_to(:destroy, OrderStatus.new) }
+   end
  end
-
 end
