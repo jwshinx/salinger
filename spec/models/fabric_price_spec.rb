0d45988c82e449b1f432664cb2fb3360b6626f4a
Joel Shin <jwshin@gmail.com>
2013-07-09 17:03:09 -0700

    rfactrd fabric mspecs

--- spec/models/fabric_price_spec.rb
+++ spec/models/fabric_price_spec.rb
@@ -11,29 +11,28 @@ describe FabricPrice do
   its(:updated_by) { should == 1 }
  end
  describe "privilege" do
-  subject { my_ability }
-  let(:my_ability) { Ability.new(@user) }
-  describe "for admin" do
-   before(:each) { @user = FactoryGirl.create(:admin_user) }
-   it { should be_able_to(:read, FabricPrice.new) }
-   it { should be_able_to(:update, FabricPrice.new) }
-   it { should be_able_to(:create, FabricPrice.new) }
-   it { should be_able_to(:destroy, FabricPrice.new) }
-  end
-  describe "for manager" do
-   before(:each) { @user = FactoryGirl.create(:manager_user) }
-   it { should be_able_to(:read, FabricPrice.new) }
-   it { should be_able_to(:update, FabricPrice.new) }
-   it { should be_able_to(:create, FabricPrice.new) }
-   it { should be_able_to(:destroy, FabricPrice.new) }
-  end
-  describe "for all else" do
-   before(:each) { @user = FactoryGirl.create(:member_user) }
-   it { should_not be_able_to(:read, FabricPrice.new) }
-   it { should_not be_able_to(:update, FabricPrice.new) }
-   it { should_not be_able_to(:create, FabricPrice.new) }
-   it { should_not be_able_to(:destroy, FabricPrice.new) }
-  end
+   subject { my_ability }
+   let(:my_ability) { Ability.new(@user) }
+   describe "for admin" do
+    before(:each) { @user = mock_model User, admin?: true }
+    it { should be_able_to(:read, FabricPrice.new) }
+    it { should be_able_to(:update, FabricPrice.new) }
+    it { should be_able_to(:create, FabricPrice.new) }
+    it { should be_able_to(:destroy, FabricPrice.new) }
+   end
+   describe "for manager" do
+    before(:each) { @user = mock_model User, manager?: true, admin?: false }
+    it { should be_able_to(:read, FabricPrice.new) }
+    it { should be_able_to(:update, FabricPrice.new) }
+    it { should be_able_to(:create, FabricPrice.new) }
+    it { should be_able_to(:destroy, FabricPrice.new) }
+   end
+   describe "for all else" do
+    before(:each) { @user = mock_model User, manager?: false, admin?: false }
+    it { should_not be_able_to(:read, FabricPrice.new) }
+    it { should_not be_able_to(:update, FabricPrice.new) }
+    it { should_not be_able_to(:create, FabricPrice.new) }
+    it { should_not be_able_to(:destroy, FabricPrice.new) }
+   end
  end
-
 end
