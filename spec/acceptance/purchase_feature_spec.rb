945eff6d532fd83b6473a397886fdadbb8c1bf0c
Joel Shin <jwshin@gmail.com>
2013-07-10 17:17:54 -0700

    added invalid discount value new-order aspecs

--- spec/acceptance/purchase_feature_spec.rb
+++ spec/acceptance/purchase_feature_spec.rb
@@ -25,7 +25,7 @@ feature 'Purchase feature', %q{
     @order.save
   end
 
-  scenario 'adding an order' do
+  scenario "adding an order with a valid discount '9.99'" do
     log_in
     visit "/new_purchase"
     fill_in 'firstname', :with => 'Jimmy'
@@ -39,6 +39,81 @@ feature 'Purchase feature', %q{
     select 'California', :from => 'address_state'
     fill_in 'address_zip', :with => '90011'
     select 'Complete', :from => 'order_status'
+    fill_in 'discount', :with => '9.99'
+    fill_in 'purchase_date_datepicker', :with => '07/05/2013'
+    select 'Raiders - $50.00', :from => 'customer_orders_attributes_0_line_items_attributes_0_product_id'
+    fill_in 'paid_amount', :with => '20'
+    click_button 'Save'
+    should_be_on "/customers/#{Customer.find_by_email('jcarter@yahoo.com').id}"
+    page.should have_content("Jimmy Carter")
+    page.should have_content("Order was successfully created.")
+    page.should have_content("$40.01")
+    page.should have_content("Discount of $9.99 applied.")
+  end
+
+  scenario 'throws error when creating order with a discount value greater than the total' do
+    log_in
+    visit "/new_purchase"
+    fill_in 'firstname', :with => 'Jimmy'
+    fill_in 'lastname', :with => 'Carter'
+    fill_in 'email', :with => 'jcarter@yahoo.com'
+    select 'Shipping', :from => 'address_type'
+    fill_in 'address_name', :with => 'Home'
+    fill_in 'address_line_one', :with => '1 Main Street'
+    fill_in 'address_suite', :with => '101'
+    fill_in 'address_city', :with => 'LA'
+    select 'California', :from => 'address_state'
+    fill_in 'address_zip', :with => '90011'
+    select 'Complete', :from => 'order_status'
+    fill_in 'discount', :with => '59.99'
+    fill_in 'purchase_date_datepicker', :with => '07/05/2013'
+    select 'Raiders - $50.00', :from => 'customer_orders_attributes_0_line_items_attributes_0_product_id'
+    fill_in 'paid_amount', :with => '20'
+    click_button 'Save'
+    page.should have_content("Discount cannot be more than the total order amount.")
+  end
+
+  scenario "adding an order of ignored non-numeric discount value 'rrr'" do
+    log_in
+    visit "/new_purchase"
+    fill_in 'firstname', :with => 'Jimmy'
+    fill_in 'lastname', :with => 'Carter'
+    fill_in 'email', :with => 'jcarter@yahoo.com'
+    select 'Shipping', :from => 'address_type'
+    fill_in 'address_name', :with => 'Home'
+    fill_in 'address_line_one', :with => '1 Main Street'
+    fill_in 'address_suite', :with => '101'
+    fill_in 'address_city', :with => 'LA'
+    select 'California', :from => 'address_state'
+    fill_in 'address_zip', :with => '90011'
+    select 'Complete', :from => 'order_status'
+    fill_in 'discount', :with => 'rrr'
+    fill_in 'purchase_date_datepicker', :with => '07/05/2013'
+    select 'Raiders - $50.00', :from => 'customer_orders_attributes_0_line_items_attributes_0_product_id'
+    fill_in 'paid_amount', :with => '20'
+    click_button 'Save'
+    should_be_on "/customers/#{Customer.find_by_email('jcarter@yahoo.com').id}"
+    page.should have_content("Jimmy Carter")
+    page.should have_content("Order was successfully created.")
+    page.should have_content("$50.00")
+  end
+ 
+
+  scenario "adding an order with a '0' discount" do
+    log_in
+    visit "/new_purchase"
+    fill_in 'firstname', :with => 'Jimmy'
+    fill_in 'lastname', :with => 'Carter'
+    fill_in 'email', :with => 'jcarter@yahoo.com'
+    select 'Shipping', :from => 'address_type'
+    fill_in 'address_name', :with => 'Home'
+    fill_in 'address_line_one', :with => '1 Main Street'
+    fill_in 'address_suite', :with => '101'
+    fill_in 'address_city', :with => 'LA'
+    select 'California', :from => 'address_state'
+    fill_in 'address_zip', :with => '90011'
+    select 'Complete', :from => 'order_status'
+    fill_in 'discount', :with => '0'
     fill_in 'purchase_date_datepicker', :with => '07/05/2013'
     select 'Raiders - $50.00', :from => 'customer_orders_attributes_0_line_items_attributes_0_product_id'
     fill_in 'paid_amount', :with => '20'
@@ -47,6 +122,7 @@ feature 'Purchase feature', %q{
     page.should have_content("Jimmy Carter")
     page.should have_content("Order was successfully created.")
   end
+
   scenario 'viewing order' do
     log_in
     visit "/orders"
