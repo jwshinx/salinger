408be9d558d230f33b8e1ed8465b2c3e8d74767d
Joel Shin <jwshin@gmail.com>
2013-07-19 17:05:32 -0700

    added pacific time zone

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+end            
 
 module Salinger
   class Application < Rails::Application
@@ -28,7 +28,7 @@ module Salinger
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
