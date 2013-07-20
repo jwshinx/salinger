ae1c7de563d1338d3e0d46f5bbfe654270313328
Joel Shin <jwshin@gmail.com>
2013-07-19 18:16:42 -0700

    rfactrd purchase process order

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,7 @@ class Order < ActiveRecord::Base
  include Moneyable 
  include Formatable  
  include Exceptions
+ include Trackable
  
  attr_accessible :created_by, :customer_id, :ispaid, :paid_amount, :paid_date, :purchase_amount, :purchase_date, :updated_by, :line_items_attributes, :fyis_attributes, :order_status_id
  has_many :line_items, :class_name => 'OrderLineItem', :dependent => :destroy
@@ -28,6 +29,17 @@ class Order < ActiveRecord::Base
      li.product.count = li.product.count - li.quantity 
      li.product.save  
    end            
+ end
+ def process_subtotals_of_line_items options, current_user, order_total
+   self.line_items.each_with_index do |oli, i|
+    set_creator_and_updater oli, current_user
+    price = Product.find(options[:customer][:orders_attributes]['0'][:line_items_attributes][i.to_s][:product_id]).price
+    oli.price = price 
+    quantity = options[:customer][:orders_attributes]['0'][:line_items_attributes][i.to_s][:quantity].to_i
+    order_total += price * quantity
+    oli.subtotal = price * quantity 
+   end   
+   order_total
  end  
  alias_method :blurb, :summary                
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -19,25 +19,17 @@ class Purchase
    @customer.orders.each do |o|
     if o.new_record?
      check_for_adequate_inventory o
-     Rails.logger.debug "---> Purchase.init 3: yes, its a new order"
      o.fyis.each { |ofyi| set_creator_and_updater ofyi, current_user }
      o.paid_amount = convert_dollars_to_cents( options[:customer][:orders_attributes]['0'][:paid_amount] )
      order_total = 0
-     set_creator_and_updater o, current_user
-     o.line_items.each_with_index do |oli, i|
-      set_creator_and_updater oli, current_user
-      price = Product.find(options[:customer][:orders_attributes]['0'][:line_items_attributes][i.to_s][:product_id]).price
-      oli.price = price
-      quantity = options[:customer][:orders_attributes]['0'][:line_items_attributes][i.to_s][:quantity].to_i
-      order_total += price * quantity
-      oli.subtotal = price * quantity
-     end
+     set_creator_and_updater o, current_user   
+     order_total = o.process_subtotals_of_line_items( options, current_user, order_total )        
      o.purchase_amount = apply_discount(o, order_total, options[:discount], current_user)
-     raise Exceptions::ExcessiveDiscountAmount if o.purchase_amount < 0
+     raise Exceptions::ExcessiveDiscountAmount if o.purchase_amount < 0                                     
      o.purchase_date = Date.strptime(options[:customer][:orders_attributes]['0'][:purchase_date], '%m/%d/%Y')
      o.paid_date = Date.strptime(
       options[:customer][:orders_attributes]['0'][:paid_date], '%m/%d/%Y'
-     ) unless options[:customer][:orders_attributes]['0'][:paid_date].blank?     
+     ) unless options[:customer][:orders_attributes]['0'][:paid_date].blank?                                       
     end
    end
  end                     
@@ -49,10 +41,14 @@ class Purchase
    @customer.todos.each { |t| set_creator_and_updater t, current_user }
    @customer.addresses.each { |a| set_creator_and_updater a, current_user }
  end  
- def apply_discount(order, total, dollars, current_user)
+ def apply_discount(order, total, dollars, current_user)    
+   Rails.logger.debug "---> Purchase.init apply_discount 1"
    return total if dollars.blank?       
+   Rails.logger.debug "---> Purchase.init apply_discount 2"
    cents_discount = convert_dollars_to_cents(dollars)
+   Rails.logger.debug "---> Purchase.init apply_discount 3 #{total} #{cents_discount}"
    set_creator_and_updater(order.fyis.build({content: "Discount of $#{dollars} applied."}), current_user) if cents_discount > 0 
+   Rails.logger.debug "---> Purchase.init apply_discount 4"
    total - cents_discount
  end
  def save
